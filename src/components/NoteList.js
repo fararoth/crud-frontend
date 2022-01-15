6b279204b04ce52290f8b12df8ea08b15c7fa9dc
Luis Farias <60280254+fariasByte@users.noreply.github.com>
2022-01-15 13:42:37 -0400

    Update NoteList.js

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,16 +1,5 @@
 import React, { Component } from "react";
 
-
-const List = (props) => (
-    <ul>
-        {
-            props.items.map((item, i) => {
-                return <li key={i}>{item}</li>
-            })
-        }
-    </ul>
-)
-
 export default class NoteList extends Component {
   constructor() {
         super();
