af27d281d84302500ebdd0f62f2e6938d38430a4
Luis Farias <60280254+fariasByte@users.noreply.github.com>
2022-01-15 17:33:09 -0400

    Update NoteList.js

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Table, Button } from 'reactstrap';
-import ModalForm from './Modal';
+import ModalForm from './modal';
 
 export default class list extends Component {
   deleteItem = (id) => {
