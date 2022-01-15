f2e2cb481144a60b8da5751cb0209333f5fe08c7
Luis Farias <60280254+fariasByte@users.noreply.github.com>
2022-01-15 13:34:02 -0400

    Update NoteList.js

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
 
+
+const List = (props) => (
+    <ul>
+        {
+            props.items.map((item, i) => {
+                return <li key={i}>{item}</li>
+            })
+        }
+    </ul>
+)
+
 export default class NoteList extends Component {
-  render() {
-    return <div>NOTHING HERE YET</div>;
-  }
+  constructor() {
+        super();
+        this.state = { items: [] };
+    }
+
+    componentDidMount() {
+        fetch('https://rickandmortyapi.com/api/character').then(result=>result.json()).then(items=>this.setState({
+            done: true,
+            items }))
+    }
+
+    render() {
+       return(
+            <div>
+                {this.state.done && this.state.items.isArray() ? (
+                    <List items={...this.state.items} />
+                ) : (
+                    <p>Cargando resultados...</p>
+                )}
+            </div>
+        )
+    }
 }
