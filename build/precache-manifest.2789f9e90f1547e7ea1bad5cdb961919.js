ed3f2a07811c1cedacb08217526eb09e30f54a92
Luis Farias <60280254+fariasByte@users.noreply.github.com>
2022-01-15 12:05:40 -0400

    Add files via upload

---
+++ build/precache-manifest.2789f9e90f1547e7ea1bad5cdb961919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6aa64005417958c6e4b8cfe46ec61cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a428e3234dabc326a26",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4f7115b1084875bf6868",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4a428e3234dabc326a26",
+    "url": "/static/js/2.bd2ed88b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.bd2ed88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7115b1084875bf6868",
+    "url": "/static/js/main.529b03ab.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
---
+++ src/components/createNote.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class createNote extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CREATE NOTE</h1>
+      </div>
+    );
+  }
+}
